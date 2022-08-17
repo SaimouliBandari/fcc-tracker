1f5980fba69cf2cf8098b67fb040996de8c3f069
SaimouliBandari <saimouli4k@gmail.com>
2022-08-18 00:11:48 +0530

    9th testcase

--- userSchema.js
+++ userSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const excercise = {
+    
     description : {type: String, required: true},
     duration: {type: Number, required: true},
     date: {type: String, required: true}
@@ -9,6 +10,7 @@ const excercise = {
 const UserSchema = mongoose.Schema({
     username : {type: String, required: true},
     excercise : [{
+        _id : false,
         description : {type: String, required: true},
         duration: {type: Number, required: true},
         date: {type: String, required: true}
